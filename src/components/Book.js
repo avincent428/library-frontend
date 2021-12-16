bd0d68d16913055eb53ccfbe5e189f3c82b0f61b
avincent428 <amielvincent@gmail.com>
2021-12-16 14:46:31 -0500

    created a book element

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Book(props) {
-  return <div className="book">book</div>;
+  return (
+    <div className="book">
+      <img
+        src="http://books.google.com/books/content?id=KL2ONx6LTV0C&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api"
+        alt="Book"
+        className="book-image"
+      />
+
+      <div className="book-info">
+        <p>The Lost Books Visual Edition</p>
+        <p>Ted Dekker</p>
+      </div>
+
+      <p className="book-availability">available</p>
+    </div>
+  );
 }
 
 export default Book;

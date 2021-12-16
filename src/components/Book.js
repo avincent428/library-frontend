27df3dae80c7cd3a5d93e2bcae3a5109b232c180
avincent428 <amielvincent@gmail.com>
2021-12-16 16:16:56 -0500

    added axios call to library db

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,20 +1,33 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 function Book(props) {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  function getData() {
+    axios.get("http://localhost:8000/books").then((res) => {
+      const bookData = res.data;
+      setBooks(bookData);
+    });
+  }
   return (
-    <div className="book">
-      <img
-        src="http://books.google.com/books/content?id=KL2ONx6LTV0C&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api"
-        alt="Book"
-        className="book-image"
-      />
-
-      <div className="book-info">
-        <p>The Lost Books Visual Edition</p>
-        <p>Ted Dekker</p>
-      </div>
-
-      <p className="book-availability">available</p>
+    <div>
+      {books.map((book) => {
+        return (
+          <div className="book" key={book._id}>
+            <img src={book.image} alt={book.title} className="book-image" />
+            <div className="book-info">
+              <p>{book.title}</p>
+              <p>{book.authors[0]}</p>
+            </div>
+            <p className="book-availability">available</p>
+          </div>
+        );
+      })}
     </div>
   );
 }

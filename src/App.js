1de771380e630590e3b7114d4d9012abe75fe8be
daniel-alarcon97 <danny.alarcon1997@gmail.com>
2021-12-16 11:38:11 -0500

    base

--- src/App.js
+++ src/App.js
@@ -19,10 +19,20 @@ function App() {
     });
   }
   return (
-    <div>
-      <header>
-        <Title />
-        <SearchBar />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React :::
+        </a>
       </header>
 
       <main>

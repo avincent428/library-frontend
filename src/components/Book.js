ad57d9420466e8b393be6c3131dc523a53364991
avincent428 <amielvincent@gmail.com>
2021-12-16 14:07:50 -0500

    created basic layout

---
+++ src/components/Book.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Book(props) {
+  return <div className="book">book</div>;
+}
+
+export default Book;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SearchBar(props) {
+  return (
+    <div>
+      <input type="text" />
+      <button type="submit">Enter </button>
+    </div>
+  );
+}
+
+export default SearchBar;

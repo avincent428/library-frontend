2a949b9a33b3eb7b01f468a6636a4974649e6fad
scottmacleod <t_mac3131@hotmail.com>
2021-12-21 12:19:29 -0600

    got delete to work, and made it so user can edit any part of the book they like

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -7,8 +7,6 @@ function Modal(props) {
     }
 
     function handleChange(event) {
-        console.log(event.target.checked);
-        console.log(props.bookIndex);
         if (props.modalInfo.available)
             props.setModalInfo({...props.modalInfo, available: !event.target.checked});
         else
@@ -18,11 +16,16 @@ function Modal(props) {
     function onClick(event, id) {
         axios.put(`http://localhost:8000/books/${id}`, props.modalInfo).then((res) => {
             const bookData = res.data;
-            console.log(bookData.available)
-            bookData.available = !bookData.available
-            console.log(bookData.available)
-            props.setBooks({...props.books, available: props.modalInfo})
-            event.preventDefault()
+            props.setBooks({...props.books, bookData});
+            event.preventDefault();
+        })
+    }
+
+    function deleteBook(event, id) {
+        axios.delete(`http://localhost:8000/books/${id}`).then((res) => {
+            const bookData = res.data;
+            props.setBooks(bookData);
+            event.preventDefault();
         })
     }
       
@@ -39,18 +42,59 @@ function Modal(props) {
                     </div>
                 </div>
                 <div className='modal-footer'>
-                    <form onChange={handleChange} >
-                        <input type="checkbox"></input>
+                    <form>
+                        <input type="checkbox" onChange={handleChange}></input>
                         <label>
                             Checked Out / In
-                            <button 
-                                className='edit-info-button' 
-                                type="submit" 
-                                onClick={(event) => onClick(event, props.modalInfo._id)} 
-                            >
-                                Edit
-                            </button>
                         </label>
+                        <div>
+                            <label>Edit Title:</label>
+                            <input type="text" onChange={(event) => props.setModalInfo({...props.modalInfo, title: event.target.value})}></input>
+                        </div>
+                        <div>
+                            <label>Edit Author:</label>
+                            <input type="text" onChange={(event) => props.setModalInfo({...props.modalInfo, authors: event.target.value})}></input>
+                        </div>
+                        <div>
+                            <label>Edit Genre:</label>
+                            <input type="text" onChange={(event) => props.setModalInfo({...props.modalInfo, genre: event.target.value})}></input>
+                        </div>
+                        <div>
+                            <label>Edit Year Published:</label>
+                            <input type="text" onChange={(event) => props.setModalInfo({...props.modalInfo, published: event.target.value})}></input>
+                        </div>
+                        <div>
+                            <label>Edit ISBN:</label>
+                            <input type="text" onChange={(event) => props.setModalInfo({...props.modalInfo, isbn: event.target.value})}></input>
+                        </div>
+                        <div>
+                            <label>Edit Format:</label>
+                            <input type="text" onChange={(event) => props.setModalInfo({...props.modalInfo, format: event.target.value})}></input>
+                        </div>
+                        <div>
+                            <label>Edit Description:</label>
+                            <textarea rows="5" columns="1" onChange={(event) => props.setModalInfo({...props.modalInfo, description: event.target.value})}></textarea>
+                        </div>
+                        <div>
+                            <label>Edit Location:</label>
+                            <input type="text" onChange={(event) => props.setModalInfo({...props.modalInfo, location: event.target.value})}></input>
+                        </div>
+                        <div>
+                            <label>Edit Image URL:</label>
+                            <input type="text" onChange={(event) => props.setModalInfo({...props.modalInfo, image: event.target.value})}></input>
+                        </div>
+                        <button 
+                            className='edit-info-button' 
+                            type="submit" 
+                            onClick={(event) => onClick(event, props.modalInfo._id)} 
+                        >
+                            Edit
+                        </button>
+                        <button 
+                            onClick={(event) => deleteBook(event, props.modalInfo._id)}
+                        >
+                            Delete Book
+                        </button>
                     </form>
                     <button className='modal-button' onClick={props.onClose}>Close</button>
                 </div>

bd1d2ff71aa90cc40af46d78dbd04e988e2cd462
daniel-alarcon97 <danny.alarcon1997@gmail.com>
2021-12-22 11:28:51 -0500

    BookDetails finally works

--- src/components/Book.js
+++ src/components/Book.js
@@ -62,18 +62,20 @@ function Book(props) {
 
       {props.filteredBooks.map((book) => {
         return (
-          <div className="book" key={book._id}>
-            <img src={book.image} alt={book.title} className="book-image" />
-            <div className="book-info">
-              <p>{book.title}</p>
-              <p>{book.authors[0]}</p>
+          <Link to={`/books/${book._id}`} key={book._id}>
+            <div className="book" key={book._id}>
+              <img src={book.image} alt={book.title} className="book-image" />
+              <div className="book-info">
+                <p>{book.title}</p>
+                <p>{book.authors[0]}</p>
+              </div>
+              {book.available ? (
+                <p className="book-availability">available</p>
+              ) : (
+                <p className="book-availability">checked out</p>
+              )}
             </div>
-            {book.available ? (
-              <p className="book-availability">available</p>
-            ) : (
-              <p className="book-availability">checked out</p>
-            )}
-          </div>
+          </Link>
         );
       })}
       <Pages />
